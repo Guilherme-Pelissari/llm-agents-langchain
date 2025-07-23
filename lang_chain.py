100225713a799e04d400aa3120069028590b4213
guilhermefiap <guipelissari2000@gmail.com>
2025-07-23 02:23:18 -0300

    Projeto LangChain com Gemini e Maritaca

---
+++ lang_chain.py
@@ -0,0 +1,49 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.chat_models import ChatMaritalk
+from langchain_core.messages import HumanMessage
+from my_models import GEMINI_FLASH, MARITACA_SABIA
+from my_keys import GEMINI_API_KEY, MARITACA_API_KEY
+from my_helper import encode_image
+from langchain.prompts import ChatPromptTemplate
+
+llm = ChatGoogleGenerativeAI(
+    api_key=GEMINI_API_KEY,
+    model=GEMINI_FLASH
+)
+
+imagem = encode_image("dados\exemplo_grafico.jpg")
+
+
+template_analisador = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Assuma que você é um analisador de imagens. A sua tarefa principal
+            consiste em: Analisar uma imagem e extrair informações importantes
+            de forma objetiva.
+
+            # FORMATO DE SAÍDA
+            Descrição da imagem: 'Coloque a sua descrição da imagem aqui'
+            """ 
+        ),
+        (
+            "user",
+            [
+                {
+                    "type" : "text",
+                    "text" : "Descreva a imagem: "
+                },
+                {
+                    "type" : "image_url",
+                    "image_url" : "data:image/jpeg;base64,{imagem}"
+                }
+            ]
+        )
+    ]
+)
+
+
+resposta = llm.invoke([mensagem])
+
+print(resposta)
